126f8b26f7d7fc4b4c5fefc0c766eda2ef38072d
Ali HAMDI <ahamdi@exoplatform.com>
2019-05-30 10:56:27 +0200

    Task : add ideation page configuration (#15)
    * Fix space creation problem
    
    * update Vue components
    
    * removed unnecessary files
    
    * Update configuration to create automatically a page for Ideation in the Intranet site

---
+++ services/src/main/java/org/exoplatform/ideation/integration/notification/NotificationUtils.java
@@ -0,0 +1,11 @@
+package org.exoplatform.ideation.integration.notification;
+
+/**
+ * Created by eXo Platform SAS.
+ *
+ * @author Ali Hamdi <ahamdi@exoplatform.com>
+ * @since 29/05/19 16:44
+ */
+
+public class NotificationUtils {
+}
